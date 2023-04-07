2aaef96b0e701abeb01db9781aaa92a0a7556d9e
Aldy Ardiansyah <127917776+Axsafacto@users.noreply.github.com>
2023-04-07 22:15:05 +0700

    Add files via upload

--- Class1.cs
+++ Class1.cs
@@ -4,32 +4,38 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp2
+namespace LatihanTaxi
 {
-    public class Class1
+    internal class Class1
     {
         public string DriverName { get; set; }
-        public string OnDuty { get; set; }
-        public string NumberOfPassenger { get; set; }
-
-
+        public bool OnDuty { get; set; }
+        public int NumberOfPassenger { get; set; }
 
         public void TaxiInfo()
         {
-            Console.WriteLine("Driver Name : {0}", DriverName);
-            Console.WriteLine("OnDuty : {0}", OnDuty);
-            Console.WriteLine("Number Of Passenger : {0}", NumberOfPassenger);
-
+            Console.WriteLine("Nama Driver : {0}", DriverName);
+            if (OnDuty == true)
+            {
+                Console.WriteLine("On duty : Yes");
+            }
+            if (OnDuty == false)
+            {
+                Console.WriteLine("On Duty : No");
+            }
+            Console.WriteLine("Number of Passenger : {0}", NumberOfPassenger);
         }
-
         public void PickUpPassenger()
         {
-            Console.WriteLine("\n {0} Sedang menjemput penumpang", DriverName);
-        }
 
+            Console.WriteLine("{0} sedang menjemput penumpang", DriverName);
+
+        }
         public void DropOffPassenger()
         {
-            Console.WriteLine(" {0} Selesai menjemput penumpang", DriverName);
+
+            Console.WriteLine("{0} selesai mengantar penumpang", DriverName);
+
         }
     }
 }
