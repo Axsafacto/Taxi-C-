dd3e8fc8dd3f6129c570b3af4eb2d3698b7228e7
Axsafacto <aldy.ardiansyah17@students.amikom.ac.id>
2023-04-07 20:13:48 +0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using ConsoleApp2;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Taxi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Class1 taxi = new Class1();
+
+            taxi.DriverName = "Wowi";
+            taxi.OnDuty = "Yes";
+            taxi.NumberOfPassenger = "10";
+
+            taxi.TaxiInfo();
+            taxi.PickUpPassenger();
+            taxi.DropOffPassenger();    
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
+
