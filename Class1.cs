31df1c91c958e691f4663c791c9a258ae6e07137
Axsafacto <aldy.ardiansyah17@students.amikom.ac.id>
2023-04-07 20:14:59 +0700

    first commit

---
+++ Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class Class1
+    {
+        public string DriverName { get; set; }
+        public string OnDuty { get; set; }
+        public string NumberOfPassenger { get; set; }
+
+
+
+        public void TaxiInfo()
+        {
+            Console.WriteLine("Driver Name : {0}", DriverName);
+            Console.WriteLine("OnDuty : {0}", OnDuty);
+            Console.WriteLine("Number Of Passenger : {0}", NumberOfPassenger);
+
+        }
+
+        public void PickUpPassenger()
+        {
+            Console.WriteLine("\n {0} Sedang menjemput penumpang", DriverName);
+        }
+
+        public void DropOffPassenger()
+        {
+            Console.WriteLine(" {0} Selesai menjemput penumpang", DriverName);
+        }
+    }
+}
